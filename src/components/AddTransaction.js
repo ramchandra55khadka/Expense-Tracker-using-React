7ac4bbf74efc9a4a9d36073b965a9ec9a908a120
Khadka <khadkarachandra981@gmil.com>
2024-08-05 21:08:49 +0545

    EXPENSE tracker

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,61 @@
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
+const AddTransaction = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+  const { addTransaction } = useContext(GlobalContext);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount,
+    };
+    addTransaction(newTransaction);
+  };
+
+  return (
+    <>
+      <h3>Add new transaction</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-control">
+          <label htmlFor="text">
+            <h3>Text</h3>
+          </label>
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter text..."
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">
+            <h3>Amount</h3>
+            (+:expense, -:income)
+          </label>
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            id="amount"
+            placeholder="Enter amount..."
+          />
+        </div>
+        <button className="btn">
+          <h4>Add transaction</h4>
+        </button>
+      </form>
+      <footer className="foot">
+        <i>
+          Created By RamchandraKhadka 2024
+          <br />
+          (ramchandrakhadka55@.com.np)
+        </i>
+      </footer>
+    </>
+  );
+};
+
+export default AddTransaction;
